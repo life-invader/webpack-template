9abc352847bd13d5231d1979af2430b52e31be91
Alexander <lav@d-element.ru>
2025-01-04 20:43:14 +0500

    chore: add webpack cfg file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import _ from 'lodash';
+import img from './image.png';
+import dataJSON from './data.json';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
@@ -6,7 +9,12 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+  const myIcon = new Image();
+  myIcon.src = img;
+  element.appendChild(myIcon);
+
   return element;
 }
 
 document.body.appendChild(component());
+console.log(dataJSON);
