b467264b0c75b6782087fdea3b3159b25355008f
Alexander <lav@d-element.ru>
2025-05-04 17:39:48 +0500

    feat: add post-css & autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,14 @@ export default (env) => {
           use: [
             isDevMode ? "style-loader" : MiniCssExtractPlugin.loader,
             "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [["autoprefixer"]],
+                },
+              },
+            },
           ],
         },
         {
